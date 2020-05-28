cabfffd22a7334bf568b688303ccd92e6c3c999e
infinit-lab <qwert200_002@163.com>
2020-05-28 17:51:59 +0800

    add decode and encode

--- utils/machine.go
+++ utils/machine.go
@@ -1,8 +1,12 @@
 package utils
 
 import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/md5"
 	"encoding/base64"
+	"encoding/hex"
 	"github.com/infinit-lab/yolanda/logutils"
 	"strings"
 )
@@ -33,4 +37,70 @@ func GetMachineFingerprint() (string, error) {
 	encode = strings.ReplaceAll(encode, "+", "-")
 	encode = strings.ReplaceAll(encode, "/", "_")
 	return encode, nil
+}
+
+func generateKey(fingerprint string) ([]byte, error) {
+	fingerprint = strings.ReplaceAll(fingerprint, "-", "+")
+	fingerprint = strings.ReplaceAll(fingerprint, "_", "/")
+	decode, err := base64.StdEncoding.DecodeString(fingerprint)
+	if err != nil {
+		logutils.Error("Failed to DecodeString. error: ", err)
+		return nil, err
+	}
+	has := md5.Sum(decode)
+	return has[:], nil
+}
+
+func Encode(fingerprint string, content []byte) (string, error) {
+	key, err := generateKey(fingerprint)
+	if err != nil {
+		return "", err
+	}
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		logutils.Error("Failed to NewCipher. error: ", err)
+		return "", err
+	}
+	iv := bytes.Repeat([]byte("1"), block.BlockSize())
+	stream := cipher.NewCTR(block, iv)
+	dst := make([]byte, len(content))
+	stream.XORKeyStream(dst, content)
+	return hex.EncodeToString(dst), nil
+}
+
+func EncodeSelf(content []byte) (string, error) {
+	key, err := GetMachineFingerprint()
+	if err != nil {
+		return "", err
+	}
+	return Encode(key, content)
+}
+
+func Decode(fingerprint string, encryptData string) ([]byte, error) {
+	data, err := hex.DecodeString(encryptData)
+	if err != nil {
+		return nil, err
+	}
+	key, err := generateKey(fingerprint)
+	if err != nil {
+		return nil, err
+	}
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		logutils.Error("Failed to NewCipher. error: ", err)
+		return nil, err
+	}
+	iv := bytes.Repeat([]byte("1"), block.BlockSize())
+	stream := cipher.NewCTR(block, iv)
+	dst := make([]byte, len(data))
+	stream.XORKeyStream(dst, data)
+	return dst, nil
+}
+
+func DecodeSelf(encryptData string) ([]byte, error) {
+	key, err := GetMachineFingerprint()
+	if err != nil {
+		return nil, err
+	}
+	return Decode(key, encryptData)
 }
--- utils/machine_test.go
+++ utils/machine_test.go
@@ -11,4 +11,29 @@ func TestGetMachineFingerprint(t *testing.T) {
 		t.Fatal("Failed to GetMachineFingerprint. error: ", err)
 	}
 	log.Println("Machine Fingerprint is ", fingerprint)
+
+	testStr := []byte("Hello World")
+	encodeData, err := Encode(fingerprint, testStr)
+	if err != nil {
+		t.Fatal("Failed to Encode. error: ", err)
+	}
+	log.Println("Encode: ", encodeData)
+
+	decodeData, err := Decode(fingerprint, encodeData)
+	if err != nil {
+		t.Fatal("Failed to Decode. error: ", err)
+	}
+	log.Println("Decode: ", string(decodeData))
+
+	encodeData, err = EncodeSelf(testStr)
+	if err != nil {
+		t.Fatal("Failed to EncodeSelf. error: ", err)
+	}
+	log.Println("Encode: ", encodeData)
+
+	decodeData, err = DecodeSelf(encodeData)
+	if err != nil {
+		t.Fatal("Failed to DecodeSelf. error: ", err)
+	}
+	log.Println("Decode: ", string(decodeData))
 }
