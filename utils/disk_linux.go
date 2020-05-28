58fa9fb9bab30d373da192b9ca7511cb8ef9213e
infinit-lab <qwert200_002@163.com>
2020-05-28 15:31:34 +0800

    add linux get disk uuid

--- utils/disk_linux.go
+++ utils/disk_linux.go
@@ -2,6 +2,66 @@
 
 package utils
 
+import (
+	"github.com/infinit-lab/yolanda/logutils"
+	"os/exec"
+	"strings"
+	"log"
+	"regexp"
+	"io/ioutil"
+)
+
 func GetDiskSerialNumber() ([]string, error) {
-	return "", nil
+	cmd := exec.Command("blkid")
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		logutils.Error("Failed to CombinedOutpu. error: ", err)
+		return nil, err
+	}
+	blkMap := make(map[string]string)
+	lines := strings.Split(string(out), "\n")
+	for _, line := range lines {
+		sections := strings.Split(line, ":")
+		if len(sections) < 2 {
+			continue;
+		}
+		reg := regexp.MustCompile("UUID=\".*\" T")
+		data := reg.Find([]byte(sections[1]))
+		if len(data) == 0 {
+			continue
+		}
+		uuid := strings.ReplaceAll(string(data), "UUID=\"", "")
+		uuid = strings.ReplaceAll(uuid, "\" T", "")
+		blkMap[sections[0]] = uuid
+	}
+
+	data, err := ioutil.ReadFile("/etc/fstab")
+	if err != nil {
+		logutils.Error("Failed to ReadFile. error: ", err)
+		return nil, err
+	}
+	lines = strings.Split(string(data), "\n")
+
+	var uuidList []string
+	for _, line := range lines {
+		if strings.Contains(line, " / ") == false || strings.Contains(line, "#") {
+			continue
+		}
+		sections := strings.Split(line, " / ")
+		if len(sections) < 2 {
+			continue;
+		}
+		section := strings.ReplaceAll(sections[0], " ", "")
+		if strings.Contains(section, "UUID=") {
+			section = strings.ReplaceAll(section, "UUID=", "")
+			uuidList = append(uuidList, section)
+		} else {
+			uuid, ok := blkMap[section]
+			if ok {
+				uuidList = append(uuidList, uuid)
+			}
+		}
+	}
+	return uuidList, nil
 }
+
